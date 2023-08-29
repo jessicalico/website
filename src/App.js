9c03009c4053cdfbda91eb38992e1bce4e62521c
jessicalico <143472500+jessicalico@users.noreply.github.com>
2023-08-29 12:33:40 -0400

    Update App.js
    added /home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
         <Route path='/projects' element={<Projects/>} />
         <Route path='/contact' element={<Contact/>} />
       </Routes>
