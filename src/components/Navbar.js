4d0b7bb8eb45bbdb3e362e49080f6477c4fbc7fa
jessicalico <143472500+jessicalico@users.noreply.github.com>
2023-09-01 01:35:46 +0000

    added about in menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,6 +38,11 @@ function Navbar() {
                 </Link>
             </li>
             <li className='nav-item'>
+                <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                    about
+                </Link>
+            </li>
+            <li className='nav-item'>
                 <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
                     projects
                 </Link>
