d8619eab37a6ca0910dc6f674eea0f466693f912
jessicalico <143472500+jessicalico@users.noreply.github.com>
2023-09-01 01:43:20 +0000

    trying to add about!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home';
+import About from './compnents/pages/About';
 import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='' element={<Home/>} />
+        <Route path='about' element={<About/>} />
         <Route path='projects' element={<Projects/>} />
         <Route path='contact' element={<Contact/>} />
       </Routes>
